eafa1dc109eb69d2f3ea95ec8fd22fa995decbc7
Alvaro Andrade <varito1@gmail.com>
2020-12-17 15:24:23 -0300

    Added frontend home, login and register pages

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
@@ -30,7 +30,9 @@ module.exports = {
     async createPost(_, { body }, context) {
       const user = checkAuth(context);
 
-      console.log('USER => ', user);
+      if (args.body.trim() === '') {
+        throw new Error('Post body must not be empty');
+      }
 
       const newPost = new Post({
         body,
@@ -40,6 +42,8 @@ module.exports = {
       });
 
       const post = await newPost.save();
+
+      context.pubsub.publish('NEW_POST', { newPost: post });
 
       return post;
     },
@@ -57,6 +61,28 @@ module.exports = {
       } catch (error) {
         throw new Error(error);
       }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+      const post = await Post.findById(postId);
+
+      if (post) {
+        if (post.likes.find(like => like.username === username)) {
+          post.likes = post.likes.filter(like => like.username !== username);
+        } else {
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString()
+          });
+        }
+        await post.save();
+        return post;
+      } else throw new UserInputError('Post not found');
+    }
+  },
+  Subscription: {
+    newPost: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator('NEW_POST')
     }
   }
 };
